4c172e073a9ad9859166f3c0341c8f996c695d55
Nicholas Varabioff <46659008+nvarabioff@users.noreply.github.com>
2025-02-25 16:23:43 -0800

    cleaning and linting.

--- src/app.py
+++ src/app.py
@@ -7,64 +7,44 @@ import pandas as pd
 
 
 df = pd.read_csv('data/raw/pokemon.csv')
-a = df[["name"]]
-a = a.rename(columns={"name": "label"})
-a["value"] = df['pokedex_number']
+a = df[['name']]
+a = a.rename(columns={'name': 'label'})
+a['value'] = df['pokedex_number']
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
-
-pokemon_data = pd.read_csv('../data/raw/pokemon.csv')
 
 # Layout
 app.layout = dbc.Container([
     html.Div([
         html.H1('Welcome to Pokédash'),
         html.P('Pokédash is your personal Pokéguide to understand your lil pocket monster'),
-        html.P("This is an app created by Agam, Albert, Nicholas and Shannon")]),
-<<<<<<< HEAD
-    html.H1('Welcome to Pokédash'),
-    html.P('Pokédash is your personal Pokéguide to understand your lil pocket monster'),
-    html.P("This is an app created by Agam, Albert, Nicholas and Shannon")]),
-    html.Label('Filter for weight in kg'),
-=======
->>>>>>> e0d30f2 (remove old components and add relevant scatterplot code. Add linting changes.)
-    html.Label('Select type'),
+        html.P('This is an app created by Agam, Albert, Nicholas, and Shannon')]),
     html.Br(),
-    dcc.Dropdown(
-        id='input_dropdown',
-        options=['Normal', 'Fire', 'Water', 'Electric', 'Grass', 'Ice',
-                 'Fighting', 'Poison', 'Ground', 'Flying', 'Psychic', 'Bug',
-                 'Rock', 'Ghost', 'Dragon', 'Dark', 'Steel', 'Fairy'],
-        multi=True,
-        placeholder='Select multiple types'
-    ),
     html.Div([
-        "Single dynamic Dropdown",
-        dcc.Dropdown(id="my-dynamic-dropdown")
+        'Single dynamic Dropdown',
+        dcc.Dropdown(id='my_dynamic_dropdown')
     ]),
     html.Br(),
-    html.Div(id='output_area'),
+    html.Div(id='output_area')
 ])
 
-options = a.to_dict(orient='records')
-
 
 @callback(
-    Output("my-dynamic-dropdown", "options"),
-    Input("my-dynamic-dropdown", "search_value")
+    Output('my_dynamic_dropdown', 'options'),
+    Input('my_dynamic_dropdown', 'search_value')
 )
 def update_options(search_value):
+    options = a.to_dict(orient='records')
     if not search_value:
         raise PreventUpdate
-    return [o for o in options if search_value in o["label"]]
+    return [o for o in options if search_value in o['label']]
 
 
-# Server side callbacks/reactivity
 @callback(
     Output('output_area', 'children'),
-    Input('input_dropdown', 'value'))
+    Input('my_dynamic_dropdown', 'value'))
 def update_output(input_value):
-    return input_value
+    return f'Pokedex Number: {input_value}'
 
 
 # Run the app/dashboard

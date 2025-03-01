1e92843dd2a1d5b511d2bc3c576e198ee9cf8113
Agam Sanghera <agamsanghera911@gmail.com>
2025-03-01 03:29:59 -0800

    feat: Added type disadvantage barchart (#41)
    * Pull updates from main to searchbar (#39)
    
    * Create search bar and loaded the dataset (#33)
    
    * Nv 0225 scatterplot (#34)
    
    * remove old components and add relevant scatterplot code.
    
    * Create search bar and loaded the dataset (#33)
    
    * remove old components and add relevant scatterplot code. Add linting changes.
    
    * cleaning and linting.
    
    * create scatterplot with custom axes and selected pokemon.
    
    * formatting.
    
    ---------
    
    Co-authored-by: Agam Sanghera <agamsanghera911@gmail.com>
    
    * Add layout and barchart (#35)
    
    * Add layout and histogram
    
    * Fixed some layout, added type images, make barchart dynamic with filter
    
    * Fix png name, added extra tooltip (#36)
    
    * Add layout and histogram
    
    * Fixed some layout, added type images, make barchart dynamic with filter
    
    * Fix png image name, added extra tooltip information for barchart
    
    * Added boxplot to dashboard (#38)
    
    * Added code for boxplot- function, callback and in layout
    
    * finished boxplot code - doesn't plot in dashboard
    
    * fixed bug, and removed legend, plot works now
    
    ---------
    
    Co-authored-by: Nicholas Varabioff <46659008+nvarabioff@users.noreply.github.com>
    Co-authored-by: Albert0011 <64696930+Albert0011@users.noreply.github.com>
    Co-authored-by: Shannon Pflueger <129323831+Shan-non@users.noreply.github.com>
    
    * Made dashboard scrollable
    
    * renamed variables to be useful
    
    * Added bar chart to show type disadvantage
    
    * CHanged type matchup to type disadvantage on the dashboard
    
    * Added requirements and server
    
    ---------
    
    Co-authored-by: Nicholas Varabioff <46659008+nvarabioff@users.noreply.github.com>
    Co-authored-by: Albert0011 <64696930+Albert0011@users.noreply.github.com>
    Co-authored-by: Shannon Pflueger <129323831+Shan-non@users.noreply.github.com>

--- src/app.py
+++ src/app.py
@@ -264,12 +264,19 @@ app.layout = dbc.Container([
         # Second Output Column
         dbc.Col([
             # First Row
-            html.Div(
-                dcc.Graph(
-                    # figure=fig
+            html.Div([
+                html.H2('Type Disadvantage'),
+                html.Div([
+                dvc.Vega(
+                    id="type_matchup",
+                    style={"minWidth": "100px", "flex": "1"},
+                    ),
+                    ],
+                    style={"display": "flex", "alignItems": "center", 'height': '100%'},
                 ),
-                style={'width': '100%', 'height': '100%', 'padding': '1vh'}
-            ),
+            html.Br(),
+            dvc.Vega(id='vstype', spec={}, style={'width': '100%'}),
+            ]),
 
             # Second Row
             html.Div([
@@ -285,7 +292,8 @@ app.layout = dbc.Container([
                     style={"minWidth": "100px", "flex": "1"},
                     ),
                     ],
-                    style={"display": "flex", "alignItems": "center"},
+
+                    style={"display": "flex", "alignItems": "center", 'height': '100%'},
                 ),
             html.Br(),
             dvc.Vega(id='boxplot', spec={}, style={'width': '100%'}),
@@ -494,9 +502,35 @@ def create_type_boxplot(x_col, selected_pokemon_id):
         tooltip="name"    # Add mark of chosen pokemon as a point in the appropriate type 
     )
 
+    
+
     # Return both charts (boxplot, and point of the selected Pokémon) layered together
     return alt.layer(base, selected_pokemon).to_dict()
+#Create dataframe containing type matchup
+df2=df[['against_bug', 'against_dark', 'against_dragon',
+       'against_electric', 'against_fairy', 'against_fight', 'against_fire',
+       'against_flying', 'against_ghost', 'against_grass', 'against_ground',
+       'against_ice', 'against_normal', 'against_poison', 'against_psychic',
+       'against_rock', 'against_steel', 'against_water','name']]
+#Flip the rows and columns to obtain each pokemon as an iterable 
+df2=df2.set_index("name").transpose()
+@callback(
+    Output("vstype", "spec"),
+    Input("type_matchup", "value"),
+    Input("pokemon_dropdown", "value")
+)
+def create_type_comparison(x_col, selected_pokemon_id):
+
+    base = alt.Chart(df2.reset_index()).mark_bar().encode(
+    x = alt.X(df.loc[df['pokedex_number']==selected_pokemon_id]['name'].to_list()[-1],
+              axis=alt.Axis(values=[0,0.5,1,1.5,2,4])), 
+    y = "index",  
+    tooltip=df.loc[df['pokedex_number']==selected_pokemon_id]['name'].to_list()[-1]
+    )
+    return alt.layer(base).configure_axis(grid=False).to_dict()
+
 
 # Run the app/dashboard
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
+    server=app.server

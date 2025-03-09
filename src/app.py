42e65405f58df0132a7c60f3615e3e94b43935d6
Shannon Pflueger <shannon.pflueger@gmail.com>
2025-03-09 15:27:03 -0700

    updated titles, y-axis type labels on pokemon weaknesses bar chart

--- src/app.py
+++ src/app.py
@@ -713,14 +713,31 @@ def create_type_comparison(x_col, selected_pokemon_id):
     """
     Creates a bar chart comparing type matchups for the selected Pokémon.
     """
-    base = alt.Chart(df2.reset_index()).mark_bar().encode(
+
+    # Remove 'against_' from each row so the y-axis looks cleaner
+    df2.index = [col.split('_')[-1] for col in df2.index]  
+
+    # Base Plot
+    base = alt.Chart(df2.reset_index(), width="container").mark_bar().encode(
         x=alt.X(
             df.loc[df['pokedex_number'] == selected_pokemon_id]['name'].to_list()[-1],
-            axis=alt.Axis(values=[0, 0.5, 1, 1.5, 2, 4])),
-        y="index",
+            axis=alt.Axis(values=[0, 0.5, 1, 1.5, 2, 4],
+                          title = "Strength Against You",
+                          labelExpr="{'0': 'Immune', '0.5': '1/2x', '1': 'Neutral', '1.5': '1.5x', '2': '2x'}[datum.value]"  # Make strength labels clearer
+                          )
+                ),
+        y=alt.Y(
+            "index", 
+            title = "Opponent Pokémon Type"
+            ),
         tooltip=df.loc[df['pokedex_number'] == selected_pokemon_id]['name'].to_list()[-1]
     )
-    return alt.layer(base).configure_axis(grid=False).to_dict()
+
+    # Add plot title
+    plot = alt.layer(base).configure_axis(grid=False).properties(
+        title = "Pokémon Type Strength Against You"
+    )
+    return plot.to_dict()
 
 # Run the app/dashboard
 if __name__ == "__main__":
